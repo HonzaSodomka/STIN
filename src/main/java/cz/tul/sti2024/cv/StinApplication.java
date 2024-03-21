380167201a0afc8a11b1cf4f1d5c7f1e5c339249
jan.sodomka <jan.sodomka@tul.cz>
2024-03-21 15:12:49 +0100

    cv04

--- src/main/java/cz/tul/sti2024/cv/StinApplication.java
+++ src/main/java/cz/tul/sti2024/cv/StinApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class StinApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(StinApplication.class, args);
 	}
